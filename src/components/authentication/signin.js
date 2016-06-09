35eb5f775ac10d7dd7d5c350a1bee403ffb8f4d6
kurtispritchard <kpritch11@gmail.com>
2016-06-09 16:06:52 -0500

    Added text inputs for username and password

--- src/components/authentication/signin.js
+++ src/components/authentication/signin.js
@@ -13,6 +13,10 @@ module.exports = React.createClass({
     render: function() {
         return <View style={styles.container}>
             <Text>Sign In</Text>
+            <Text style={styles.label}>Username:</Text>
+            <TextInput style={styles.input}></TextInput>
+            <Text style={styles.label}>Password:</Text>
+            <TextInput secureTextEntry={true} style={styles.input}></TextInput>
         </View>
     }
 });
@@ -22,5 +26,18 @@ var styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    input: {
+        padding: 4,
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 5,
+        margin: 5,
+        width: 200,
+        alignSelf: 'center'
+    },
+    label: {
+        fontSize: 18
     }
 });
