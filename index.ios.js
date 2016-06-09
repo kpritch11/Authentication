cdcb67416ba264d87dad8132c738dd1a8c4011f5
kurtispritchard <kpritch11@gmail.com>
2016-06-09 15:08:31 -0500

    Changed ios and android index files to use main.js component

--- index.ios.js
+++ index.ios.js
@@ -1,23 +1,10 @@
 // Import the code we need
 import React from 'react';
+import Main from './src/main';
 
 import {
-    AppRegistry,
-    StyleSheet,
-    View
+    AppRegistry
 } from 'react-native';
 
-// Create a react component
-var Authentication = React.createClass({
-    render: function() {
-        return <View></View>
-    }
-});
-
-// Style the react component
-var styles = StyleSheet.create({
-
-});
-
 // Show the react component on the screen
-AppRegistry.registerComponent('authentication', () => Authentication);
+AppRegistry.registerComponent('authentication', () => Main);
