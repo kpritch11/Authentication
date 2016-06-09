dcb261a1746809883df1815506a9ab83ad16cdea
kurtispritchard <kpritch11@gmail.com>
2016-06-09 14:04:57 -0500

    Initial commit

---
+++ index.android.js
@@ -0,0 +1,23 @@
+// Import the code we need
+import React from 'react';
+
+import {
+    AppRegistry,
+    StyleSheet,
+    View
+} from 'react-native';
+
+// Create a react component
+var Authentication = React.createClass({
+    render: function() {
+        return <View></View>
+    }
+});
+
+// Style the react component
+var styles = StyleSheet.create({
+
+});
+
+// Show the react component on the screen
+AppRegistry.registerComponent('authentication', () => Authentication);
