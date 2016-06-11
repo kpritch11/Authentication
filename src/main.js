93969eb1e2aa0ebdd0b6732e1d0d1f571d96f354
kurtispritchard <kpritch11@gmail.com>
2016-06-11 16:43:18 -0500

    Setup Navigator

--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ import SignIn from './components/authentication/signin';
 import Parse from 'parse/react-native';
 
 import {
-    StyleSheet,
-    Text,
-    View
+    Navigator,
+    StyleSheet
 } from 'react-native';
+
+var ROUTES = {
+    signIn: SignIn
+};
 
 // Create a react component
 module.exports = React.createClass({
@@ -15,18 +18,23 @@ module.exports = React.createClass({
         Parse.initialize('authentication830472991', 'authenticationKLnFEon5E8t20c7');
         Parse.serverURL = 'https://kpritch-authentication.herokuapp.com/parse';
     },
+    renderScene: function(route, navigator) {
+        var Component = ROUTES[route.name];
+        return <Component></Component>
+    },
     render: function() {
-        return <View style={styles.container}>
-            <SignIn></SignIn>
-        </View>
+        return <Navigator
+            style={styles.container}
+            initialRoute={{name: 'signIn'}}
+            renderScene={this.renderScene}
+            configureScene={() => { return Navigator.SceneConfigs.FloatFromRight; }}>
+        </Navigator>
     }
 });
 
 // Style the react component
 var styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        flex: 1
     }
 });
